f1a5b8933ed28ffb03a5f973e785e54903057605
Ryan Frazier <ryanpfrazier@gmail.com>
2022-09-22 18:06:35 -0400

    Working app

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ if (app.get('env') == 'development') {
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
--- mongoose-db.js
+++ mongoose-db.js
@@ -45,12 +45,13 @@ console.log("Using Mongo URI " + mongoUri);
 mongoose.connect(mongoUri);
 
 User = mongoose.model('User');
-User.find({ username: 'admin@snyk.io' }).exec(function (err, users) {
+User.find({ username: 'admin@snyk.io' }).exec(function(err, users) {
   console.log(users);
-  if (users.length === 0) {
+  if (users == undefined || users.length === 0) {
     console.log('no admin');
-    new User({ username: 'admin@snyk.io', password: 'SuperSecretPassword' }).save(function (err, user, count) {
+    new User({ username: 'admin@snyk.io', password: 'SuperSecretPassword' }).save(function(err, user, count) {
       if (err) {
+        console.log(err)
         console.log('error saving admin user');
       }
     });
